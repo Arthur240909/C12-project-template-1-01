a35d3b3e30e60d2b6dff0f30cf8ec648c97d197b
Arthur240909 <arthurrodriguesgerhard@gmail.com>
2024-02-01 18:47:05 -0300

    update

--- C12-project-template-main/sketch.js
+++ C12-project-template-main/sketch.js
@@ -3,30 +3,30 @@ var pathImg,boyImg;
 var i;
 
 function preload(){
-  //loadImage (carregarImagem) da pista)
-  //loadAnimation (carregarAnimação) de corrida para o menino
+pathImg = loadImage("path.png");
+  boyImg = loadAnimation("Runner-1png","Runner-2png")
  
 }
 
 function setup(){
   
   createCanvas(400,400);
- //crie um sprite para a pista 
-//adicione uma imagem para a pista
-//Faça com que a pista seja um fundo que se move dando a ela velocity Y.
+path=createSprite(200,200);
+path.addImage(pathImg);
+path.velocityY=4;
 path.scale=1.2;
 
-//crie um sprite de menino
-//adicione uma animação de corrida para ele
+boy=createSprite(180,340,30,30);
 boy.scale=0.08;
+boy.scale=addAnimation("JakeRunning",boyImg);
   
 //crie um limite à esquerda
 leftBoundary=createSprite(0,0,100,800);
-//defina visibilidade como falsa para o limite à esquerda
+leftBoundary.visible=false;
 
 //crie um limite à direita
 rightBoundary=createSprite(410,0,100,800);
-//defina visibilidade como falsa para o limite à direita
+rightBoundary.visible = false;
 }
 
 function draw() {
@@ -34,7 +34,8 @@ function draw() {
   path.velocityY = 4;
   
   // mover o menino com o mouse usando mouseX
-  
+  boy.x = World.mouseX;
+
   edges= createEdgeSprites();
   boy.collide(edges[3]);
   // colidir o menino com os limites invisíveis da esquerda e da direita
